776f24869be560d30a16291207b32747834e2183
bajpayeeaum <133734666+bajpayeeaum@users.noreply.github.com>
2024-02-27 21:45:20 -0500

    Update client.py

--- answer3/client.py
+++ answer3/client.py
@@ -3,7 +3,19 @@ import threading
 import pickle
 
 class ChatClient:
+    """
+    A simple chat client that connects to a chat server to send and receive messages.
+    """
+
     def __init__(self, host, port, username):
+        """
+        Initialize the chat client.
+
+        Parameters:
+        - host (str): The host address of the chat server.
+        - port (int): The port number of the chat server.
+        - username (str): The username of the client.
+        """
         self.host = host
         self.port = port
         self.username = username
@@ -12,6 +24,9 @@ class ChatClient:
         self.socket.sendall(pickle.dumps(username))
 
     def receive_messages(self):
+        """
+        Receive messages from the chat server and print them.
+        """
         try:
             while True:
                 data = self.socket.recv(4096)
@@ -25,6 +40,9 @@ class ChatClient:
             self.socket.close()
 
     def send_message(self):
+        """
+        Send messages typed by the user to the chat server.
+        """
         try:
             while True:
                 message = input()
@@ -36,6 +54,9 @@ class ChatClient:
             self.socket.close()
 
     def start(self):
+        """
+        Start the client by creating and starting threads for sending and receiving messages.
+        """
         receive_thread = threading.Thread(target=self.receive_messages)
         receive_thread.start()
         send_thread = threading.Thread(target=self.send_message)
@@ -45,4 +66,5 @@ if __name__ == "__main__":
     username = input("Enter your username: ")
     client = ChatClient('localhost', 5555, username)
     client.start()
+
 
